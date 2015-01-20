680b6debf2c846e85c6c6399632d9035ae7bcd06
tooto1985 <tooto1985@gmail.com>
2015-01-20 16:54:38 +0800

    modify

--- test-include.js
+++ test-include.js
@@ -1,6 +1,16 @@
 (function() {
 	if ((/android/i.test(navigator.userAgent) || /iphone/i.test(navigator.userAgent)) && $("#header").length > 0) {
-		$(".ui-page").animate({"top":30},800);
+		var $downloadapp = $(".downloadapp");
+		if ($downloadapp.length < 1) {
+			$("body").prepend($("<div>").addClass("downloadapp"));
+			$downloadapp = $(".downloadapp");
+		}
+
+		$downloadapp.html("<div><h1>test</h1></div>");
+
+
+		$(".downloadapp").height(0).animate({"height":60},800);
+		$(".ui-page").animate({"top":60},800);
 
 	}
 })();
