18055d1e02f74a3b070a59a931bd2b1bd443968b
tooto1985 <tooto1985@gmail.com>
2015-01-21 10:40:57 +0800

    modify

--- test-include.js
+++ test-include.js
@@ -11,9 +11,19 @@
 			$downloadapp = $(".downloadapp");
 		}
 		var html = "<div>";
+		html += "<table border=\"0\">";
+		html += "<tr>";
+		html += "<td>"
 		html += "<img src=\"https://lh4.ggpht.com/1yLfmfOeaYWsTFHV0gyNGszIGhB2GudxM8D68T3GEJzfeljKaaANA14dL-jWTuIKTQ=w40-rw\">";
+		html += "</td>"
+		html += "<td>"
 		html += "<span>下載手機版APP瀏覽，活動訊息即時通知！</span>";
+		html += "</td>"
+		html += "<td>"
 		html += "<a href=\"http://app.hishop.com.tw/?download=ja\">立即下載</a>";
+		html += "</td>"				
+		html += "</tr>";
+		html += "</table>";
 		html += "</div>";
 		$downloadapp.html(html);
 		$(".downloadapp").animate({
