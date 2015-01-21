1ef598652daa101c3d5a6b1a0649c41c2157f526
tooto1985 <tooto1985@gmail.com>
2015-01-21 10:33:37 +0800

    modify

--- test-include.js
+++ test-include.js
@@ -2,16 +2,21 @@
 	if ((/android/i.test(navigator.userAgent) || /iphone/i.test(navigator.userAgent)) && $("#header").length > 0) {
 		var $downloadapp = $(".downloadapp");
 		if ($downloadapp.length < 1) {
+			$("head").append($("<link>").attr({
+				rel: "stylesheet",
+				type: "text/css",
+				href: "https://rawgit.com/tooto1985/project-ja/master/test-include.css"
+			}));
 			$("body").prepend($("<div>").addClass("downloadapp"));
 			$downloadapp = $(".downloadapp");
 		}
 		var html = "<div>";
 		html += "<img src=\"https://lh4.ggpht.com/1yLfmfOeaYWsTFHV0gyNGszIGhB2GudxM8D68T3GEJzfeljKaaANA14dL-jWTuIKTQ=w40-rw\">";
 		html += "<span>下載手機版APP瀏覽，活動訊息即時通知！</span>";
-		html += "<a href=\"http://app.hishop.com.tw/?download=ja\"></a>";
+		html += "<a href=\"http://app.hishop.com.tw/?download=ja\">立即下載</a>";
 		html += "</div>";
 		$downloadapp.html(html);
-		$(".downloadapp").height(0).css("overflow", "hidden").animate({
+		$(".downloadapp").animate({
 			"height": 60
 		});
 		$(".ui-page").animate({
